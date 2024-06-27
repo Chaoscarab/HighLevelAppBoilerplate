093ee4b38de8309e000c46ed0025131f1bf08f26
Brandon VanFossen <bgvfossen@gmail.com>
2024-06-27 11:43:11 -0400

    added url get

--- src/app.js
+++ src/app.js
@@ -1,8 +1,36 @@
 import React from "react";
 
 const App = () => {
+    const [url, setUrl] = useState('')
+
+
+
+    useEffect(async() => {
+        try{
+            let svrRes = await fetch('http://localhost:3000/url')
+            setUrl(svrRes.json())
+        }catch{
+            setUrl('error')
+        }
+    })
+
+let elements;
+
+if(url === 'error'){
+    elements = <div>failed to fetch URL</div>
+}else if(url !== ""){
+    elements = <a href={url}>
+            <button>subscribe</button>
+        </a>
+}else{
+    elements = <div>loading...</div>
+}
+
+
     return(
-        <div>hello world</div>
+        <div>
+            {elements}
+        </div>
     )
 }
 
